52385cbab7596cef06e1a16a677c9258acb83303
Reuben Poharama <reuben.hubbubstudios.com>
2016-02-10 18:37:17 +1300

    Better Interaction

--- QuantumV0.5/QuantumV0.5.ino
+++ QuantumV0.5/QuantumV0.5.ino
@@ -4,24 +4,24 @@
 
 /*
 controlPins are for the first transistors and control the direction of current.
-ledPins are for the second transistors and control the voltage level for the LEDs.
-The ledPins and controlPins have to be opposite sides of the H bridge.
+pins are for the second transistors and control the voltage level for the LEDs.
+The pins and controlPins have to be opposite sides of the H bridge.
 For example, controlPin 0 can only be used with ledPin 0 (pin3), 
   ControlPin 1 with LEDpin 1, etc. 
-Each H bridge controls one strand of LEDs with 2 controlPins and 2 ledPins .
+Each H bridge controls one strand of LEDs with 2 controlPins and 2 pins .
 */
 
 // analogue out pins: 3, 4, 5, 6, 9, 10, 20, 21, 22,
 // control  out pins: 0, 1, 2, 7, 8, 11, 12, 14, 15,
 
-int ledPins[] = {3,4,5,6,9,10,20,21,22}; 
-int controlPins[] = {0,1,2,7,8,11,12,14,15}; 
-int brightness[9]; // Stores the brightness for each LED strip
-int fade[9];       // Stores how fast the leds change brightness
-bool up[9];        // fading up or down
-int pinCount = 9;     // how many pins are used for led transistors
+int pins[] = {3,4,5,6,9,10,20,21}; 
 
-int IRS = 23;         // pin for reading the IR Sensor
+float brightness[8]; // Stores the brightness for each LED strip
+float fade[8];       // Stores how fast the leds change brightness
+bool up[8];          // fading up or down
+int pinCount = 8;     // how many pins are used for led transistors
+
+int IRS = 23;         // pin for reading the IR Sensor (can also add 22)
 int sensIn;
 int dist;
 int distDiffMin = 30;   // minimum difference in distance to trigger
@@ -35,14 +35,10 @@ int pprevDist;
 void setup() {
   Serial.begin(115200);
   
-  // set controlPins as outputs 
-  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
-    pinMode(controlPins[thisPin], OUTPUT);
-  }
   
-  // set ledPins as outputs
+  // set pins as outputs
   for (int thisPin = 0; thisPin < pinCount; thisPin++) {
-    pinMode(ledPins[thisPin], OUTPUT);
+    pinMode(pins[thisPin], OUTPUT);
     brightness[thisPin] = random(10,245);  // Initialise with random brightness
   }
   
@@ -56,34 +52,30 @@ void setup() {
 //--------------------------start of loop----------------------------
 
 void loop() {
-  // set even controlPins to high and even ledPins to voltage
+  // set even pins to voltage
   for (int thisPin = 0; thisPin < pinCount; thisPin+=2) {
-    digitalWrite(controlPins[thisPin],HIGH);            
-    analogWrite(ledPins[thisPin], brightness[thisPin]);
+    analogWrite(pins[thisPin], brightness[thisPin]);
   }
-  delay(5);
+  delay(8);
 
   // Set everything low again
   for (int thisPin = 0; thisPin < pinCount; thisPin+=2) {
-    digitalWrite(controlPins[thisPin],LOW);            
-    analogWrite(ledPins[thisPin], 0); 
+    analogWrite(pins[thisPin], 0); 
   }
-  delay(1);
 
 
-  // set odd controlPins to high and odd ledPins to voltage
+
+  // set odd pins to voltage
   for (int thisPin = 1; thisPin < pinCount; thisPin+=2) {
-    digitalWrite(controlPins[thisPin],HIGH);
-    analogWrite(ledPins[thisPin], brightness[thisPin]);
+    analogWrite(pins[thisPin], brightness[thisPin]);
   }
-  delay(5);
+  delay(8);
 
   // Set everything low again 
   for (int thisPin = 1; thisPin < pinCount; thisPin+=2) {
-    digitalWrite(controlPins[thisPin],LOW);
-    analogWrite(ledPins[thisPin], 0);
+    analogWrite(pins[thisPin], 0);
   }
-  delay(1);
+  
 
 
   // Read pot to get fade speed
@@ -97,7 +89,7 @@ void loop() {
 
   
   Serial.print(" - ");
-  Serial.println(dist);
+  Serial.print(dist);
 
   distDiffMin = prevDist*0.2;
 
@@ -108,7 +100,7 @@ void loop() {
     }
   }
   else if(prevDist > pprevDist+distDiffMin && prevDist < dist-distDiffMin){
-    Serial.println("trig");
+    Serial.println("--------trigger");
     for (int thisPin = 0; thisPin < pinCount; thisPin++) {
       fade[thisPin] = 120;
     }
@@ -130,14 +122,23 @@ void loop() {
 
   
   for (int thisPin = 0; thisPin < pinCount; thisPin++) {
-    if(fade[thisPin]>dist*0.01) fade[thisPin] = fade[thisPin]-1;  // lower fade speed
+    // Move fade speed towards the closeness
+    if(fade[thisPin]>(500-dist)*0.05+1) fade[thisPin] = fade[thisPin]-(thisPin*0.1+0.5); 
+    else if(fade[thisPin]<(500-dist)*0.05+1) fade[thisPin] = fade[thisPin]+0.8; 
+
   }
+
+  
+  
   pprevDist = prevDist;
   prevDist = dist;
   
 
   
   fadeAll();
+  if(millis() % 60000 <100) blinkSeq();
+
+  Serial.println(" ");
 }
 
 
@@ -146,28 +147,28 @@ void loop() {
 
 
 
-void fadeStrip(int strip){
+void fadeStrip(int pin){
   
 
-  int bright = brightness[strip]; //read brightness from the array
+  int bright = brightness[pin]; //read brightness from the array
   
   if(bright < 0){  //When dim, change  fade direction to up
     bright = 0;
-    up[strip] = true;
+    up[pin] = true;
   }
   else if (bright > 255){ //When bright, change fade direction to down
     bright = 255;
-    up[strip] = false;
+    up[pin] = false;
   }
 
-  if(up[strip]){  //When fading up, add fade amount
-    bright += fade[strip];
+  if(up[pin]){  //When fading up, add fade amount
+    bright += fade[pin];
   }
   else{
-    bright -=fade[strip];  //When fading down, minus fade amount
+    bright -=fade[pin];  //When fading down, minus fade amount
   }
   
-  brightness[strip] = bright;  //Save to the array
+  brightness[pin] = bright+(random(-1,2));  //Save to the array with some randomness
 }
 
 
@@ -177,5 +178,29 @@ void fadeAll(){
   for (int thisPin = 0; thisPin < pinCount; thisPin++) {
     fadeStrip(thisPin);
   }
+}
+
+void blinkSeq(){
+  Serial.println("Wait for it...");
+  // wait for 2 seconds
+  delay(2000);
+
+  // run sequence
+  for (int seqStep = 0; seqStep < pinCount; seqStep++){
+    // Make sure all are off
+    for (int thisPin = 0; thisPin < pinCount; thisPin++){
+      analogWrite(pins[thisPin], 0);
+    }
+    //turn on one pin
+    analogWrite(pins[seqStep], 255);
+    delay(100);
+    Serial.print("BAM! ");
+  }
+  delay(1000);
+  // set all brightness levels to 0
+  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+    brightness[thisPin] = 0;
+  }
+  
 }
 
